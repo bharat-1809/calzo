3a021ba76a8c9000845e36953af27682cdc42f50
Bharat Sharma <bharat.sharma1809@gmail.com>
2020-03-24 14:00:31 +0530

    Login Screen 2

---
+++ fooddelivery_app/lib/components/LoginScreenComp/LoginInputBox.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Dimensions
+} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import Colors from "../../constants/colors";
+
+const width = Dimensions.get("window").width;
+const height = Dimensions.get("window").height;
+
+function LoginInput(props) {
+  return (
+    <KeyboardAvoidingView
+      enabled
+      behavior="padding"
+      keyboardVerticalOffset={10}
+    >
+      <View style={styles.inputBox}>
+        <View style={styles.icon}>
+          <MaterialCommunityIcons
+            name={props.iconName}
+            size={31}
+            color={Colors.loginbuttonText}
+          />
+        </View>
+        <TextInput
+          style={styles.inputText}
+          placeholder={props.placeholder}
+          placeholderTextColor={"#ffffff"}
+          onChangeText={props.onChangeText}
+          value={props.value}
+          keyboardType={props.keyboardType}
+          secureTextEntry={props.secureTextEntry}
+        />
+      </View>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  inputBox: {
+    flexDirection: "row"
+  },
+  icon: {
+    marginBottom: (0.023 * height),
+    backgroundColor: "#ffffff",
+    left: 0.1268 * width,
+    alignItems: "center",
+    borderRadius: 100,
+    height: 0.066 * height,
+    width: 0.066 * height,
+    alignContent: "center",
+    justifyContent: "center"
+  },
+  inputText: {
+    marginBottom: (0.023 * height),
+    backgroundColor: "rgba(255,255,255,0.16)",
+    borderRadius: 70,
+    width: 0.746 * width,
+    height: 0.066 * height,
+    fontSize: 18,
+    fontWeight: "400",
+    textAlign: "center",
+    color: Colors.background
+  }
+});
+export default LoginInput;
