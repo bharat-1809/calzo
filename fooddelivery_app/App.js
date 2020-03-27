f0a3de90c408669e28487a30bac47d492c4db05d
Bharat Sharma <bharat.sharma1809@gmail.com>
2020-03-27 19:10:30 +0530

    Updated fonts

--- fooddelivery_app/App.js
+++ fooddelivery_app/App.js
@@ -1,13 +1,26 @@
-import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View
-} from "react-native";
-import MainNavigator from './lib/navigation/MainNavigator';
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+import MainNavigator from "./lib/navigation/MainNavigator";
+
+const getFonts = () =>
+  Font.loadAsync({
+    "comicSans-Regular": require('./assets/fonts/OpenSans-Regular.ttf'),
+    "comicSans-Bold": require('./assets/fonts/OpenSans-Bold.ttf'),
+    "comicSans-SemiBold": require('./assets/fonts/OpenSans-SemiBold.ttf'),
+  });
 
 export default function App() {
-  return ( < MainNavigator / > );
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (fontsLoaded) {
+    return <MainNavigator />;
+  } else {
+    return (
+      <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
