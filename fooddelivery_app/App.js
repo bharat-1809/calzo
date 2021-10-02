b6c5b79d1b4330cf788440b92b14812fadf91600
Aryan Vijaywargia <61878162+aryanVijaywargia@users.noreply.github.com>
2021-10-03 00:27:33 +0530

    Update App.js

--- fooddelivery_app/App.js
+++ fooddelivery_app/App.js
@@ -38,4 +38,11 @@ export default function App() {
   }
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
